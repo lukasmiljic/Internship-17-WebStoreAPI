d0ff102fde809ba14d729742f7d526816e2579b8
lukasmiljic <smiljicluka13@gmail.com>
2024-04-04 14:40:00 +0200

    generated resources

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { CartsModule } from './carts/carts.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule],
+  imports: [PrismaModule, ProductsModule, UsersModule, CartsModule, WishlistsModule],
   controllers: [AppController],
   providers: [AppService],
 })
